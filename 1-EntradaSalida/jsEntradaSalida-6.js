a528c20b71a77c3174860b683b873041a3fdec64
alumno <alumno>
2019-02-08 20:35:58 -0300

    trabajos

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+var uno
+var dos
+uno = document.getElementById("numeroUno").value ;
+dos = document.getElementById("numeroDos").value ;
+parseInt(uno) ;
+parseInt(dos);
+alert( uno + dos);
 }
 
