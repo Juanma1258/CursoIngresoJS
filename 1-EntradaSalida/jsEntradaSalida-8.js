3078c70b9bb522f1a7a2784e39c74eed75124557
alumno <alumno>
2019-02-08 21:56:16 -0300

    trabajos3

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -2,7 +2,15 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
+var dividendo;
+var divisor;
+var division;
 function SacarResto()
 {
-	
+	dividendo = document.getElementById("numeroDividendo").value;
+    divisor = document.getElementById("numeroDivisor").value;
+    dividendo = parseInt(dividendo);
+    divisor = parseInt(divisor);
+    division = dividendo % divisor;
+    alert("La división es " + division);
 }
