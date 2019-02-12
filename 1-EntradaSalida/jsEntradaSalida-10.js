d656b2d42e4d7037945f201f551d82dfd8f3e211
unknown <juanma2061258@gmail.com>
2019-02-12 00:34:28 -0300

    trabajos6

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    sueldo = document.getElementById("sueldo").value;
+    sueldo = parseInt(sueldo);
+    aumento = sueldo * 0.1;
+    sueldoconAumento = sueldo + aumento;
+    document.getElementById("resultado").value = sueldoconAumento;
 }
