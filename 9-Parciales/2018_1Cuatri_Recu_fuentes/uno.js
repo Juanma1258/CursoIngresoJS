66aca397b626b107fae8c90dc0ae756f411a6c75
alumno <alumno>
2019-02-15 19:39:43 -0300

    retamozojm.ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+var peso1;
+var peso2;
+var mascota1;
+var mascota2;
+var pesoTotal;
+peso1 = parseInt(peso1);
+peso2 = parseInt(peso2);
+pesoTotal = peso1 + peso2;
+alert("tenés dos mascotas: " + mascota1 + " y" + mascota2 + ",que pesan: " + peso1 + " y" + peso2 + " kilos, la suma de los kilos es: " + pesoTotal);
 
 }
