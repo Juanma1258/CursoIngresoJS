48109c6e92ee3a10e9eafb79c3f59814fa7d9af7
alumno <alumno>
2019-02-15 20:18:27 -0300

    retamozojm.ejercicio3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+var gasto1;
+var gasto2;
+var gasto3;
+var gastoTotal
+var resultadofinal
+gasto1 = prompt("Pepito gastó ");
+gasto2 = prompt("Gastón gastó ");
+gasto3 = prompt("Juan gastó ");
+gasto1 = parseInt(gasto1);
+gasto2 = parseInt(gasto2);
+gasto3 = parseInt(gasto3);
+gastoTotal = parseInt(gastoTotal);
+gastoTotal = gasto1 + gasto2 + gasto3;
+resultadofinal = gastoTotal / 3;
+document.getElementById("elPrecioFinal").value = resultadofinal;
+
 
 }
