97f75d4a8280f82ab6f80ff5caad56a3ddb93434
alumno <alumno>
2019-02-15 19:51:16 -0300

    retamozojm.ejercicio1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -6,9 +6,13 @@ var peso2;
 var mascota1;
 var mascota2;
 var pesoTotal;
+mascota1 = "Bobby";
+mascota2 = "Sultán";
+peso1 = 15;
+peso2 = 16;
 peso1 = parseInt(peso1);
 peso2 = parseInt(peso2);
 pesoTotal = peso1 + peso2;
-alert("tenés dos mascotas: " + mascota1 + " y" + mascota2 + ",que pesan: " + peso1 + " y" + peso2 + " kilos, la suma de los kilos es: " + pesoTotal);
+alert("tenés dos mascotas: " + mascota1 + " y " + mascota2 + ",que pesan: " + peso1 + " y " + peso2 + " kilos, la suma de los kilos es: " + pesoTotal);
 
 }
