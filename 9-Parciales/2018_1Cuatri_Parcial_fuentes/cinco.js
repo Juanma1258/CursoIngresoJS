12b4b233bdf07693519a009ad6bb774add5c861d
alumno <alumno>
2019-03-02 21:45:00 -0300

    lostrabajoshechos2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
 
+var planetas;
+
+planetas = prompt("ingrese el nombre de un planeta");
+
+switch(planetas){
+    case "mercurio":
+    case "venus":
+    alert("acá hace más calor");
+    break;
+    case "tierra":
+    alert("acá vivimos");
+    break;
+    case "urano":
+    case "neptuno":
+    alert("acá hace más frío");
+    break;
+    case "marte":
+    case "jupiter":
+    case "saturno":
+    case "pluton":
+    default:
+    alert("no está en la lista");
+    break;
+}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
 
+var num1;
+var num2;
+var suma;
+var resta;
+num1 = prompt("ingrese el num");
+num2 = prompt("ingrese el num");
+if( num1 == num2){
+    alert( num1 + num2 );
+}
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+resta = num1 - num2;
+suma = num2 + num1;
+
+if(num1 > num2){
+    alert(resta);
+}
+if( num2 > num1){
+    alert(suma);
+    if(suma > 10){
+        alert(suma +" se pasa de 10");
+    }
+}
 }
