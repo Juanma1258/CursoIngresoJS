00eda38534a6c3bc365bfb8f4a66e657cb3c6b5d
Juanma1258 <47339892+Juanma1258@users.noreply.github.com>
2019-07-12 22:13:19 -0300

    PARCIALES 12/07/19

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,32 +1,30 @@
-function mostrar() {
+function mostrar() 
+{
+var unNumero;
+var otroNumero;
+var resultado;
 
-    var unNumero;
-    var otroNumero;
-    var suma;
+unNumero = prompt("ingrese un numero");
+otroNumero = prompt("ingrese otro numero");
 
-    unNumero = prompt("ingrese el num");
-    otroNumero = prompt("ingrese el num");
-
-    unNumero = parseInt(unNumero);
-    otroNumero = parseInt(otroNumero);
-    suma = unNumero + otroNumero;
-
-    if (unNumero == otroNumero) {
-        alert(unNumero + "" + otroNumero);
-    }
-    else {
-        if (unNumero > otroNumero) {
-            alert(unNumero - otroNumero);
+if(unNumero == otroNumero){
+    resultado = unNumero + otroNumero;
+//concateno
+    }else{
+        if(unNumero > otroNumero){
+            resultado = unNumero - otroNumero;
+        //resto
+        }else{
+        //sumo
+            unNumero = parseInt(unNumero);
+            otroNumero = parseInt(otroNumero);
+            resultado = unNumero + otroNumero;
         }
-        else {
-
-            alert(otroNumero + unNumero);
-
-
-            if (suma > 10) {
-                alert(suma + " se pasa de 10")
-            }
-
+        if(resultado > 10){
+            resultado = "el resultado es " + resultado + " y supera los 10";
         }
     }
+
+    alert(resultado);
+
 }
