f1c729b5318c4bdf019606d04aa21e76c1716d1b
Juanma1258 <47339892+Juanma1258@users.noreply.github.com>
2019-07-13 12:32:02 -0300

    RECUPERATORIO 13 julio 2019

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,18 +1,33 @@
 function mostrar()
 {
-var cantidad;
-var precio;
-var tarjeta;
-var descuento;
-cantidad = prompt("cantidad");
-precio = prompt("precio");
-tarjeta = confirm("tarjeta");
-cantidad = parseInt(cantidad);
-precio = parseInt(precio);
-if(cantidad > 2){
-    descuento = 0.1;
-}
-if(precio > 2000){
-    descuento
-}
+    var primerNumero;
+    var segundoNumero;
+    var resultado;
+
+    primerNumero = prompt("ingrese un numero");
+    segundoNumero = prompt("ingrese otro numero");
+
+    if(primerNumero == segundoNumero)
+    {
+        resultado = primerNumero + segundoNumero;
+    }
+    else
+    {
+
+        if(primerNumero > segundoNumero)
+        {
+            resultado = primerNumero / segundoNumero;
+        }
+        else
+        {
+            primerNumero = parseInt(primerNumero);
+            segundoNumero = parseInt(segundoNumero);
+            resultado = primerNumero + segundoNumero;
+            if(resultado < 50)
+            {
+                resultado = " dsasaddsa " + resultado + " dsadasdadsadsa"
+            }
+        }
+    } 
+    alert(resultado);
 }
